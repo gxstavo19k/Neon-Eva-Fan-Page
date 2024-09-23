08b5542de8b1cc06b7670bc30e861c0fb0e031d1
gustavo19k <164823912+gustavo19k@users.noreply.github.com>
2024-09-22 21:07:57 -0300

    `feat(UX)`: adiciona um Loader para melhora da UX - adiciona diretamente ao app.js     - Adiciona um módulo exclusivo de Loader

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 import "./global.css";
 import { AppMain } from "@components/AppMain.js";
+import { SpinnerLoader } from "@modules/SpinnerLoader.js";
 
 addEventListener("DOMContentLoaded", () => {
+    SpinnerLoader();
     const app = document.querySelector('#app');
     const containerMain = AppMain();
     app.appendChild(containerMain);
---
+++ src/modules/SpinnerLoader.js
@@ -0,0 +1,22 @@
+import "@styles/spinner.css";
+export function SpinnerLoader() {
+    const spinner = document.createElement('div');
+    spinner.id = 'spinner';
+    document.body.appendChild(spinner);
+    document.body.classList.add('loading');
+
+    function checkIfLoaded() {
+        const allLoaded = document.readyState === 'complete';
+
+        if (allLoaded) {
+            document.body.classList.remove('loading');
+            spinner.remove();
+            console.log("Todos os recursos foram carregados.");
+        } else {
+            setTimeout(checkIfLoaded, 100);
+        }
+    }
+
+
+    checkIfLoaded();
+}
